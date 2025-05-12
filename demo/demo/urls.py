5240e02fa5af154c87272e4c8f20124a20a8b320
Haruko386 <tryeverypossible@163.com>
2025-05-12 17:23:39 +0800

    学习了应用的创建 📖

--- demo/demo/urls.py
+++ demo/demo/urls.py
@@ -17,7 +17,9 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path
+from app01.views import hello
 
 urlpatterns = [
+    path("hello/", hello),
     path("admin/", admin.site.urls),
 ]
